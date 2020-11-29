c178c28b60d8e9b65ead0559a933f4e6a33029fc
Juliusz Chroboczek <jch@irif.fr>
2020-11-29 17:00:12 +0100

    Add test for description marshalling/unmarshalling.

--- group/group_test.go
+++ group/group_test.go
@@ -1,6 +1,8 @@
 package group
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -18,5 +20,39 @@ func TestJSTime(t *testing.T) {
 	delta := tm.Sub(tm2)
 	if delta < -time.Millisecond/2 || delta > time.Millisecond/2 {
 		t.Errorf("Delta %v, %v, %v", delta, tm, tm2)
+	}
+}
+
+func TestDescriptionJSON(t *testing.T) {
+	d := `
+{
+    "op":[{"username": "jch","password": "topsecret"}],
+    "max-history-age": 10,
+    "allow-subgroups": true,
+    "presenter":[
+        {"user": "john", "password": "secret"},
+        {}
+    ]
+}`
+
+	var dd description
+	err := json.Unmarshal([]byte(d), &dd)
+	if err != nil {
+		t.Fatalf("unmarshal: %v", err)
+	}
+
+	ddd, err := json.Marshal(dd)
+	if err != nil {
+		t.Fatalf("marshal: %v", err)
+	}
+
+	var dddd description
+	err = json.Unmarshal([]byte(ddd), &dddd)
+	if err != nil {
+		t.Fatalf("unmarshal: %v", err)
+	}
+
+	if !reflect.DeepEqual(dd, dddd) {
+		t.Errorf("Got %v, expected %v", dddd, dd)
 	}
 }
