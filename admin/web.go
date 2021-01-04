47465eb4464b54f5391d00dcf13187e84a6c9f21
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-04 20:06:29 +0100

    pprof.

--- admin/web.go
+++ admin/web.go
@@ -5,12 +5,22 @@ import (
 	"log"
 	"net/http"
 
+	"net/http/pprof"
+
 	"github.com/jech/galene/stats"
 )
 
 func New() http.Handler {
 	s := http.NewServeMux()
 	s.HandleFunc("/stats/groups", GroupeHandler)
+
+	s.HandleFunc("/pprof/cmdline", pprof.Cmdline)
+	s.HandleFunc("/pprof/profile", pprof.Profile)
+	s.HandleFunc("/pprof/symbol", pprof.Symbol)
+	s.HandleFunc("/pprof/trace", pprof.Trace)
+	//s.HandleFunc("/pprof/", pprof.Index)
+
+	s.HandleFunc("/", HomeHandler)
 
 	return s
 }
@@ -21,4 +31,16 @@ func GroupeHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(`
+    __ _  __ _| | ___ _ __   ___
+   / _' |/ _' | |/ _ \ '_ \ / _ \
+  | (_| | (_| | |  __/ | | |  __/
+   \__, |\__,_|_|\___|_| |_|\___|
+   |___/
+
+`))
 }
