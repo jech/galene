3b0cdc6c1d89b82bd97bac2da7fa823c82258d67
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-06 11:04:03 +0100

    Cache password. Test basic auth.

--- admin/basic_auth.go
+++ admin/basic_auth.go
@@ -3,11 +3,14 @@ package admin
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/jech/galene/group"
 )
 
 func BasicAuthMiddleware(password *group.Password, handler http.Handler) http.HandlerFunc {
+	cachedPw := ""
+	lock := sync.RWMutex{}
 	return func(w http.ResponseWriter, r *http.Request) {
 		name, pw, ok := r.BasicAuth()
 		if !ok || name != "admin" {
@@ -15,15 +18,26 @@ func BasicAuthMiddleware(password *group.Password, handler http.Handler) http.Ha
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		ok, err := password.Match(pw)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if !ok {
-			w.WriteHeader(http.StatusForbidden)
-			return
+		lock.RLock()
+		if cachedPw == "" || pw != cachedPw {
+			ok, err := password.Match(pw)
+			if err != nil {
+				log.Println(err)
+				lock.RUnlock()
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			if !ok {
+				lock.RUnlock()
+				w.WriteHeader(http.StatusForbidden)
+				return
+			}
+			lock.RUnlock()
+			lock.Lock()
+			cachedPw = pw
+			lock.Unlock()
+		} else {
+			lock.RUnlock()
 		}
 		handler.ServeHTTP(w, r)
 	}
--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/pion/webrtc/v3 v3.0.3
 	github.com/prometheus/client_golang v1.9.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	github.com/stretchr/testify v1.6.1
 )
