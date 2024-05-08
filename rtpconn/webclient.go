7a837624e366a789f5cb2ea1d34caa59617e9ed8
Juliusz Chroboczek <jch@irif.fr>
2024-05-08 15:38:51 +0200

    Enforce clearchat permissions in the server.
    It was only being enforced in the client.

--- rtpconn/webclient.go
+++ rtpconn/webclient.go
@@ -1612,6 +1612,9 @@ func handleClientMessage(c *webClient, m clientMessage) error {
 		}
 		switch m.Kind {
 		case "clearchat":
+			if !member("op", c.permissions) {
+				return c.error(group.UserError("not authorised"))
+			}
 			g.ClearChatHistory()
 			m := clientMessage{
 				Type:       "usermessage",
